6346fd966ad030c04716adb9f120c66becf94fb4
wallansag <wallans.gomes@fatec.sp.gov.br>
2025-06-06 00:27:48 -0300

    .

---
+++ ex1.py
@@ -0,0 +1,13 @@
+
+ 
+nome= input("Digite seu nome")
+idade= int (input("digite sua idade"))
+altura=float (input("digite sua altura"))
+estudante=input("você é estudante? sim ou não")
+print(nome)
+print(idade)
+print(altura)
+if(estudante=="sim"):
+     print("é estudante")
+else: print(" não é estudante")
+ 
---
+++ ex2.py
@@ -0,0 +1,9 @@
+int = 1
+float = 1.2
+string = "1"
+boolean = True
+ 
+print(type(int))
+print(type(float))
+print(type(string))
+print(type(boolean))
